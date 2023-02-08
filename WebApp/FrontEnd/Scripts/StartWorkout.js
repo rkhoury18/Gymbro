d3553e0d2671284062b4be1303b00468f42efdba
krishagrawal112 <krishagrawal112@gmail.com>
2023-02-08 15:52:50 +0000

    Fixed workout.js

--- WebApp/FrontEnd/Scripts/StartWorkout.js
+++ WebApp/FrontEnd/Scripts/StartWorkout.js
@@ -21,6 +21,84 @@ Object.size = function(obj) {
     return size;
   };
 
+function createExercise(i, workout) {
+    ex_obj = workout["exec" + String(i + 1)]
+            
+    name_button = document.createElement("button");
+    name_text = document.createTextNode(ex_obj.name)
+    w_div =  document.createElement("div")
+    r_div = document.createElement("div")
+    s_div = document.createElement("div")
+    rst_div = document.createElement("div")
+    w = document.createElement("input")
+    r = document.createElement("input")
+    s = document.createElement("input")
+    rst = document.createElement("input")
+    w_label = document.createElement("label")
+    r_label = document.createElement("label")
+    s_label = document.createElement("label")
+    rst_label = document.createElement("label")
+    w_text = document.createTextNode("Weights")
+    r_text = document.createTextNode("Reps")
+    s_text = document.createTextNode("Sets")
+    rst_text = document.createTextNode("Rest")
+    
+    w.value = workout.weight
+    r.value = workout.reps
+    s.value = workout.sets
+    rst.value = workout.rest
+
+    if (i == 0){ 
+        name_button.setAttribute("class", "exerciseBlue")
+    }
+    else{
+        name_button.setAttribute("class", "exerciseGrey")
+    }
+    w_div.setAttribute("class", "form__group_a field")
+    r_div.setAttribute("class", "form__group_b field")
+    s_div.setAttribute("class", "form__group_c field")
+    rst_div.setAttribute("class", "form__group_d field")
+    w.setAttribute("class", "form__field_a")
+    r.setAttribute("class", "form__field_b")
+    s.setAttribute("class", "form__field_c")
+    rst.setAttribute("class", "form__field_d")
+    w_label.setAttribute("class", "form__label_a")
+    r_label.setAttribute("class", "form__label_b")
+    s_label.setAttribute("class", "form__label_c")
+    rst_label.setAttribute("class", "form__label_d")
+    w_text = document.createTextNode("Weights")
+    r_text = document.createTextNode("Reps")
+    s_text = document.createTextNode("Sets")
+    rst_text = document.createTextNode("Rest")
+    
+    name_button.style.top = String(5 + 25*i) + "%"
+    w_div.style.top = String(13 + 25*i) + "%"
+    r_div.style.top  = String(13 + 25*i) + "%"
+    s_div.style.top  = String(13 + 25*i) + "%"
+    rst_div.style.top  = String(13 + 25*i) + "%"
+
+    name_button.appendChild(name_text)
+    w_label.appendChild(w_text)
+    r_label.appendChild(r_text)
+    s_label.appendChild(s_text)
+    rst_label.appendChild(rst_text)
+
+    w_div.appendChild(w_label)
+    w_div.appendChild(w)
+    r_div.appendChild(r_label)
+    r_div.appendChild(r)
+    s_div.appendChild(s_label)
+    s_div.appendChild(s)
+    rst_div.appendChild(rst_label)
+    rst_div.appendChild(rst)
+
+    document.appendChild(w_div)
+    document.appendChild(r_div)
+    document.appendChild(s_div)
+    document.appendChild(rst_div)
+}
+
+
 window.onload = function() {
 
     workout_promise = rcvdatajson("/rcv/workout")
@@ -31,80 +109,7 @@ window.onload = function() {
 
 
         for (let i = 0; i < num_exercises; i++){
-            ex_obj = workout["exec" + String(i + 1)]
-            
-            name = document.createElement("button");
-            name_text = document.createTextNode(ex_obj.name)
-            w_div =  document.createElement("div")
-            r_div = document.createElement("div")
-            s_div = document.createElement("div")
-            rst_div = document.createElement("div")
-            w = document.createElement("input")
-            r = document.createElement("input")
-            s = document.createElement("input")
-            rst = document.createElement("input")
-            w_label = document.createElement("label")
-            r_label = document.createElement("label")
-            s_label = document.createElement("label")
-            rst_label = document.createElement("label")
-            w_text = document.createTextNode("Weights")
-            r_text = document.createTextNode("Reps")
-            s_text = document.createTextNode("Sets")
-            rst_text = document.createTextNode("Rest")
-            
-            w.value = workout.weight
-            r.value = workout.reps
-            s.value = workout.sets
-            rst.value = workout.rest
-
-            if (i == 0){ 
-                name.setAttribute("class", "exerciseBlue")
-            }
-            else{
-                name.setAttribute("class", "exerciseGrey")
-            }
-            w_div.setAttribute("class", "form__group_a field")
-            r_div.setAttribute("class", "form__group_b field")
-            s_div.setAttribute("class", "form__group_c field")
-            rst_div.setAttribute("class", "form__group_d field")
-            w.setAttribute("class", "form__field_a")
-            r.setAttribute("class", "form__field_b")
-            s.setAttribute("class", "form__field_c")
-            rst.setAttribute("class", "form__field_d")
-            w_label.setAttribute("class", "form__label_a")
-            r_label.setAttribute("class", "form__label_b")
-            s_label.setAttribute("class", "form__label_c")
-            rst_label.setAttribute("class", "form__label_d")
-            w_text = document.createTextNode("Weights")
-            r_text = document.createTextNode("Reps")
-            s_text = document.createTextNode("Sets")
-            rst_text = document.createTextNode("Rest")
-            
-            // name.style.top = 
-            // w_div.style.top 
-            // r_div.style.top  = 
-            // s_div.style.top  =
-            // rst_div.style.top  = 
-
-            name.appendChild(name_text)
-            w_label.appendChild(w_text)
-            r_label.appendChild(r_text)
-            s_label.appendChild(s_text)
-            rst_label.appendChild(rst_text)
-
-            w_div.appendChild(w_label)
-            w_div.appendChild(w)
-            r_div.appendChild(r_label)
-            r_div.appendChild(r)
-            s_div.appendChild(s_label)
-            s_div.appendChild(s)
-            rst_div.appendChild(rst_label)
-            rst_div.appendChild(rst)
-
-            document.appendChild(w_div)
-            document.appendChild(r_div)
-            document.appendChild(s_div)
-            document.appendChild(rst_div)
+            createExercise(i, workout)
         }
     })
 }
--- WebApp/FrontEnd/Scripts/workout.js
+++ WebApp/FrontEnd/Scripts/workout.js
@@ -20,6 +20,97 @@ function senddatajson(json,url){
 //   xhr.send(str);
 //   }  
 
+function createWorkoutElement(i, workout_names){
+    var delete_div = document.createElement("div");
+    var newWorkout = document.createElement("button");
+    var newStart = document.createElement("button");
+    var newModify = document.createElement("button");
+    var newDelete = document.createElement("button");
+    var workout_text = document.createTextNode(workout_names[i].name);
+    const start_text = document.createTextNode("Start");
+    const modify_text = document.createTextNode("Modify");
+    const delete_text = document.createTextNode("X")
+
+    newWorkout.setAttribute("class", "push")
+    newStart.setAttribute("class", "start1")
+    //newStart.setAttribute("onclick", "window.location.href='/start_workout'")
+    newModify.setAttribute("class", "modify1")
+    newDelete.setAttribute("class", "delete1")
+    delete_div.setAttribute("class", "parent")
+
+    newWorkout.id = "workout" + String(i + 1)
+    newStart.id = "start" + String(i + 1)
+    newModify.id = "modify" + String(i + 1)
+    newDelete.id = "delete" + String(i + 1)
+    delete_div.id =  "delete_d" + String(i + 1)
+
+    newWorkout.style.top = String(40 + i*20) + "%";
+    newStart.style.top = String(42.5 + i*20) + "%";
+    newModify.style.top = String(42.5 + i*20) + "%";
+    newDelete.style.top = String(42.5 + i*20) + "%";
+
+    newWorkout.appendChild(workout_text);
+    newStart.appendChild(start_text);
+    newModify.appendChild(modify_text);
+    newDelete.appendChild(delete_text);
+    delete_div.appendChild(newDelete);
+    document.body.appendChild(newWorkout);
+    document.body.appendChild(newStart);
+    document.body.appendChild(newModify);
+    document.body.appendChild(delete_div);
+
+    //Add listener for start button
+    newStart.addEventListener("click", function() {
+      console.log("start id:", newStart.id)
+      workout_num = parseInt(newStart.id.slice(-1)) - 1;
+      console.log(workout_num)
+      var wrkt_name = workout_names[workout_num]
+      console.log(wrkt_name)
+      console.log("Start clicked")
+      senddatajson(wrkt_name, "/client/workout/start")
+    })
+    //Add listeners for delete button
+    newDelete.addEventListener("click", function() {
+      workout_num = parseInt(newDelete.id.slice(-1)) - 1;
+      workout = document.getElementById("workout" + String(workout_num))
+      start = document.getElementById("start" + String(workout_num))
+      modify = document.getElementById("modify" + String(workout_num))
+      delete_b = document.getElementById("delete" + String(workout_num))
+      delete_div = document.getElementById("delete_div" + String(workout_num))
+
+      removal(workout);
+      removal(start);
+      removal(sets);
+      removal(modify);
+      removal(delete_b);
+      removal(delete_div);
+  
+      for (let j = workout_num + 1; j <= workout_names.length; j+=1){
+          workout = document.getElementById("workout" + String(j))
+          start = document.getElementById("start" + String(j))
+          modify = document.getElementById("modify" + String(j))
+          delete_b = document.getElementById("delete" + String(j))
+          delete_div = document.getElementById("delete_div" + String(j))
+  
+          workout.id = "workout" + String(j - 1)
+          start.id = "start" + String(j - 1)
+          modify.id = "modify" + String(j - 1)
+          delete_b.id = "delete" + String(j - 1)
+          delete_div.id = "delete_div" + String(j - 1)
+
+          workout.style.top = String(40 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
+          start.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
+          modify.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
+          delete_b.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
+      }
+      senddatajson(workout_names[workout_num], "/client/workout/delete")
+      workout_names.splice(workout_num);
+
+    }) //Closing delete listener
+}
+
+
+
 window.onload = function() {
 
   //Create objects for each workout
@@ -32,95 +123,10 @@ window.onload = function() {
     // console.log(workout_names.length)
     for (let i = 0; i < workout_names.length; i++){
       //TODO: Change to functions
-      var delete_div = document.createElement("div");
-      var newWorkout = document.createElement("button");
-      var newStart = document.createElement("button");
-      var newModify = document.createElement("button");
-      var newDelete = document.createElement("button");
-      var workout_text = document.createTextNode(workout_names[i].name);
-      const start_text = document.createTextNode("Start");
-      const modify_text = document.createTextNode("Modify");
-      const delete_text = document.createTextNode("X")
-
-      newWorkout.setAttribute("class", "push")
-      newStart.setAttribute("class", "start1")
-      //newStart.setAttribute("onclick", "window.location.href='/start_workout'")
-      newModify.setAttribute("class", "modify1")
-      newDelete.setAttribute("class", "delete1")
-      delete_div.setAttribute("class", "parent")
-
-      newWorkout.id = "workout" + String(i + 1)
-      newStart.id = "start" + String(i + 1)
-      newModify.id = "modify" + String(i + 1)
-      newDelete.id = "delete" + String(i + 1)
-      delete_div.id =  "delete_d" + String(i + 1)
-
-      newWorkout.style.top = String(40 + i*20) + "%";
-      newStart.style.top = String(42.5 + i*20) + "%";
-      newModify.style.top = String(42.5 + i*20) + "%";
-      newDelete.style.top = String(42.5 + i*20) + "%";
-
-      newWorkout.appendChild(workout_text);
-      newStart.appendChild(start_text);
-      newModify.appendChild(modify_text);
-      newDelete.appendChild(delete_text);
-      delete_div.appendChild(newDelete);
-      document.body.appendChild(newWorkout);
-      document.body.appendChild(newStart);
-      document.body.appendChild(newModify);
-      document.body.appendChild(delete_div);
-
-      //Add listener for start button
-      newStart.addEventListener("click", function() {
-        console.log("start id:", newStart.id)
-        workout_num = parseInt(newStart.id.slice(-1)) - 1;
-        console.log(workout_num)
-        var wrkt_name = workout_names[workout_num]
-        console.log(wrkt_name)
-        console.log("Start clicked")
-        senddatajson(wrkt_name, "/client/workout/start")
-      })
-      //Add listeners for delete button
-      newDelete.addEventListener("click", function() {
-        workout_num = parseInt(newDelete.id.slice(-1)) - 1;
-        workout = document.getElementById("workout" + String(workout_num))
-        start = document.getElementById("start" + String(workout_num))
-        modify = document.getElementById("modify" + String(workout_num))
-        delete_b = document.getElementById("delete" + String(workout_num))
-        delete_div = document.getElementById("delete_div" + String(workout_num))
-
-        removal(workout);
-        removal(start);
-        removal(sets);
-        removal(modify);
-        removal(delete_b);
-        removal(delete_div);
-    
-        for (let j = workout_num + 1; j <= workout_names.length; j+=1){
-            workout = document.getElementById("workout" + String(j))
-            start = document.getElementById("start" + String(j))
-            modify = document.getElementById("modify" + String(j))
-            delete_b = document.getElementById("delete" + String(j))
-            delete_div = document.getElementById("delete_div" + String(j))
-    
-            workout.id = "workout" + String(j - 1)
-            start.id = "start" + String(j - 1)
-            modify.id = "modify" + String(j - 1)
-            delete_b.id = "delete" + String(j - 1)
-            delete_div.id = "delete_div" + String(j - 1)
-
-            workout.style.top = String(40 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
-            start.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
-            modify.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
-            delete_b.style.top = String(42.5 + 20*(j - 2)) + "%"; //TODO: Make this not hardcoded
-        }
-        senddatajson(workout_names[workout_num], "/client/workout/delete")
-        workout_names.splice(workout_num);
-
-    }) //Closing delete listener
+      createWorkoutElement(i, workout_names)
+      
   } //Closing for loop
   }); //Closing of then
-
 
 };
   
