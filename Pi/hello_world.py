788cb96301d9c19f736491c2b9caaf7ab34c54a8
krishagrawal112 <krishagrawal112@gmail.com>
2023-02-17 10:04:30 +0000

    Submission-grade code

--- Pi/hello_world.py
+++
@@ -1,8 +0,0 @@
-import requests as rq
-
-url = 'http://13.41.78.246:3000/hello_world'
-hello_world = {'message': 'hello world'}
-
-x = rq.post(url, json=hello_world)
-y = x.json()
-print(y["message"])
